0d81070dbd911a1c2b7ea00893caca4e233bd84d
20127557Long <74892972+20127557Long@users.noreply.github.com>
2020-11-23 13:57:05 +0800

    Add files via upload

---
+++ B7.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int m, n;
+	int s = 0, dem = 0;
+	do
+	{
+		cout << "Nhap 2 so nguyen m va n (m<n): " << endl;
+		cin >> m >> n;
+	} while (m >= n);
+	for (int i = m;i <= n && i >= m;i++)
+	{
+		if (i % 2 == 0) {
+			dem++;
+			s = s + i;
+		}
+	}
+	cout << "so luong chan la: " << dem << endl;
+	cout << "tong cac so chan la: " << s << endl;
+	return 0;
+}
